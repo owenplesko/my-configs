e2df1c45063997c9475b009fdf090943592d22b2
owen plesko <owenplesko@gmail.com>
2025-06-12 16:19:01 -0400

    nvim blink completion

---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,15 @@
+return {
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  version = '1.*',
+
+  opts = {
+    keymap = { preset = 'default' },
+    appearance = {
+      nerd_font_variant = 'mono'
+    },
+    signature = { enabled = true },
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+}
