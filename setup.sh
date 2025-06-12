7d2beadb6879195dbd6e071d380a18f66a87f34b
owen plesko <owenplesko@gmail.com>
2025-06-12 16:35:55 -0400

    mac setup initial

---
+++ setup.sh
@@ -0,0 +1,40 @@
+
+#!/bin/sh
+
+# ---- Homebrew install check ----
+if ! command -v brew >/dev/null 2>&1; then
+  echo "Homebrew not found. Installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# ---- Install packages ----
+
+# GCC
+brew install gcc
+
+# Nerd font: Agave NF Mono
+brew tap homebrew/cask-fonts
+brew install --cask font-agave-nerd-font
+
+# Starship prompt
+brew install starship
+
+# WezTerm
+brew install --cask wezterm
+
+# Neovim
+brew install neovim
+
+# Git
+brew install git
+
+# ---- Symlinks ----
+
+CONFIG_DIR="$HOME/my-configs"
+
+# WezTerm config
+ln -sf "$CONFIG_DIR/wezterm/.wezterm.lua" "$HOME/.wezterm.lua"
+
+# Neovim config
+mkdir -p "$HOME/.config"
+ln -sf "$CONFIG_DIR/nvim" "$HOME/.config/nvim"
