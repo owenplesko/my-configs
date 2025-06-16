631dea3bc8b53a03c13c9cc2fde4e3712c19c88d
Owen Plesko <owenplesko@gmail.com>
2025-06-15 20:17:09 -0400

    added keybind for formatting buffer

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -3,3 +3,9 @@ vim.keymap.set("", "<Up>", "<Nop>")
 vim.keymap.set("", "<Down>", "<Nop>")
 vim.keymap.set("", "<Left>", "<Nop>")
 vim.keymap.set("", "<Right>", "<Nop>")
+
+
+-- Formatting
+vim.keymap.set("n", "<leader>f", function()
+  vim.lsp.buf.format()
+end, { desc = "Format file" })
