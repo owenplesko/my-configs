ca85dd419ba35543cba87ac87d3eedac6e30f010
Owen Plesko <owenplesko@gmail.com>
2025-08-04 18:25:11 -0400

    moved remaps to be in their designated plugin fil

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,20 +1,35 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "gopls",
-        "ts_ls",
-        "tailwindcss",
-        "cssls",
-        "html",
-        "pyright",
-        "lua_ls",
-      }
-    },
-    dependencies = {
-      { "williamboman/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-  },
+
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		{ "williamboman/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"gopls",
+				"ts_ls",
+				"tailwindcss",
+				"cssls",
+				"html",
+				"pyright",
+				"lua_ls",
+			},
+		})
+
+		-- Formatting
+		vim.keymap.set("n", "<leader>f", function()
+			vim.lsp.buf.format()
+			vim.lsp.buf.code_action({
+				context = { only = { "source.organizeImports" } },
+				apply = true,
+			})
+		end, { desc = "Format file" })
+
+		-- Code actions
+		vim.keymap.set("n", "<leader>ca", function()
+			vim.lsp.buf.code_action()
+		end, { desc = "View Code Actions" })
+	end,
 }
